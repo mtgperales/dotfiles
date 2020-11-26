ddd3421f104d58adacb0b65e9c6e691c8a2f553a
Tristan Perales <mtgperales@tip.edu.ph>
2020-11-26 14:19:42 +0800

    Backup dotfiles

--- restore.sh
+++ restore.sh
@@ -2,13 +2,15 @@
 
 print "Hello [$USER]"
 print "Restoring i3 config from dotfiles backup"
-cp .config/i3/config ~/.config/i3/config
+mkdir ~/.config/i3
+cp -R .config/i3/ ~/.config/i3/
 print "Done"
 print "Restoring i3status config from dotfiles backup"
 mkdir ~/.config/i3status
-cp .config/i3status/config ~/.config/i3status/config
+cp -R .config/i3status ~/.config/i3status
 print "Done"
 print "Restoring i3blocks config from dotfiles backup"
+mkdir ~/.config/i3blocks
 cp -R .config/i3blocks ~/.config/i3blocks
 print "Done"
 print "Restoring .zshrc config from dotfiles backup"
@@ -19,5 +21,5 @@ source ~/.zshrc
 print "Done"
 print "Restore Complete"
 print "Restarting i3"
-i3 restart
+i3-msg restart
 print "Done"

21febebff2d5d546c8f66d39f22b837c3f0c1285
Tristan Perales <mtgperales@tip.edu.ph>
2020-11-26 14:13:41 +0800

    Backup dotfiles

--- backup.sh
+++ backup.sh
@@ -2,12 +2,15 @@
 
 print "Hello [$USER]"
 print "Copying i3 config from ~/.config/i3 to dotfiles folder"
+mkdir .config/i3
 cp ~/.config/i3/config ~/dotfiles/.config/i3/config
 print "Backup done"
 print "Copying i3status config from ~/.config/i3status to dotfiles folder"
+mkdir .config/i3status
 cp ~/.config/i3status/config ~/dotfiles/.config/i3status/config
 print "Backup done"
 print "Copying i3blocks config from ~/.config/i3blocks to dotfiles folder"
+mkdir .config/i3blocks
 cp -R ~/.config/i3blocks dotfiles/.config/i3blocks
 print "Done"
 print "Copying .zshrc config from ~/.zshrc to dotfiles folder"
